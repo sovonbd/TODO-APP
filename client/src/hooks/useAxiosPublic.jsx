f7017dd29db847d215e99cd109e02cec1fae073c
MR. Sovon <sovonsdp@gmail.com>
2024-04-11 03:42:48 -0500

    dist

--- client/src/hooks/useAxiosPublic.jsx
+++ client/src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://ec2-18-227-81-244.us-east-2.compute.amazonaws.com:5000",
+  baseURL: "http://ec2-18-118-163-90.us-east-2.compute.amazonaws.com:5000",
   // baseURL: "http://localhost:5000",
   withCredentials: true,
 });
